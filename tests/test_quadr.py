29b90fc3267b5138c0943bcbc7f93c0b5d31cc89
Evgeni Burovski <evgeny.burovskiy@gmail.com>
2018-10-30 20:11:03 +0300

    TST: add a test with overflow

--- tests/test_quadr.py
+++ tests/test_quadr.py
@@ -15,3 +15,8 @@ def test_two_cmplx_conj_roots():
     assert_allclose(quad_solve(0, 1), (1j, -1j))
 
 
+def test_large_b():
+    # Use Viet's theorem
+    b, c = 1e20, 1
+    x1, x2 = quad_solve(b, c)
+    assert_allclose(x1*x2, c)
